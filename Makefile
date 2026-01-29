5e24933fa8cee4afd02c5fae7aa20ed35bcfbe08
Nasiba <65603709+nrnasyrova@users.noreply.github.com>
2026-01-29 18:26:52 +0100

    added support for commands and default

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+-include  
+export
+
 .PHONY: run
 run:
 	go run cmd/bot/main.go
--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+) 
 
 func main() {
-	fmt.Println("Hello World")
+	token := os.Getenv("TOKEN")
+	
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+			
+			var msg tgbotapi.MessageConfig
+
+			switch update.Message.Command() {
+				case "help":
+					msg = processHelpCommand(update.Message)
+				case "list":
+					msg = processListCommand(update.Message)
+				default:
+					msg = processDefaultBehavior(update.Message)
+			}
+		
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
+}
+
+func processHelpCommand(inputMessage *tgbotapi.Message) tgbotapi.MessageConfig {
+	return tgbotapi.NewMessage(inputMessage.Chat.ID,
+		 "/help - help\n"+
+		 	"/list - list products",
+		)
+
+}
+
+func processDefaultBehavior(inputMessage *tgbotapi.Message) tgbotapi.MessageConfig {
+	return tgbotapi.NewMessage(inputMessage.Chat.ID, "luv u")
+}
+
+func processListCommand(inputMessage *tgbotapi.Message) tgbotapi.MessageConfig {
+	return tgbotapi.NewMessage(inputMessage.Chat.ID, "in process")
 }
